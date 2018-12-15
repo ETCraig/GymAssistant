92a89fe8bdd6345879e2934956fa1ec6059ab1ba
unknown <devethancraig@gmail.com>
2018-12-14 22:32:42 -0700

    Actions - Reducers - Validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Container} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import jwt_decoded from 'jwt-decode';
+import {clearCurrentUser} from './actions/userActions';
+import {Provider} from 'react-redux';
+import setAuthToken from './utils/setAuthToken';
+import {setCurrentUser, logoutUser} from './actions/authctions';
+import store from './store';
+
+if(localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decoded(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+  const currentTime = Date.now() / 3000;
+
+  if(decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    store.dispatch(clearCurrentUser());
+    window.location.href = '/login'
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Container>
+              
+            </Container>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ client/src/reducers/index.js
@@ -0,0 +1,12 @@
+import {combineReducers} from 'redux';
+import authReducer from './authReducer';
+import errorReducer from './errorReducer';
+import goalReducer from './goalReducer';
+import userReducer from './goalReducer';
+
+export default combineReducers({
+    auth: authReducer,
+    errors: errorReducer,
+    goals: goalReducer,
+    user: userReducer
+})
