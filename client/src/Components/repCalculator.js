f0086e4d6c5a03dab8a41191dbf62e952cd9842d
unknown <devethancraig@gmail.com>
2018-12-21 17:44:47 -0700

    repCalculator Started

--- client/src/Components/repCalculator.js
+++ client/src/Components/repCalculator.js
@@ -1,10 +1,40 @@
 import React, {Component} from 'react';
 
+import {Button, FormGroup, Input, } from 'reactstrap';
+var repMax = require('rep-max');
+
 class repCalculator extends Component {
+    constructor() {
+        super() 
+            
+        this.state = {
+            weight: '',
+            repetition: ''
+        }
+    }
+    onchangeWeight(value) {
+        this.setState({weight: value});
+    }
+    onChangeRepetition(value) {
+        this.setState({repetition: value});
+    }
     render() {
         return(
             <div>
-                <h1>REP Calculator</h1>
+                <h1>1RM CALCULATOR</h1>
+                <FormGroup>
+                    <Input 
+                        placeholder='Weight' 
+                        value={this.state.weight}
+                        onChange={(e) => this.onchangeWeight(e.target.value)} 
+                    />
+                    <Input 
+                        placeholder='Reps' 
+                        value={this.state.repetition}
+                        onChange={(e) => this.onChangeRepetition(e.target.value)} 
+                    />
+                    <Button>Calculate Max</Button>
+                </FormGroup>
             </div>
         );
     }
--- client/src/Components/wtCalculator.js
+++ client/src/Components/wtCalculator.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 class wtCalculator extends Component {
     render() {
+        
         return(
             <div>
                 <h1>Weight Calculator</h1>
